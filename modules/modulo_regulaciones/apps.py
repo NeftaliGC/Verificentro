a5f80bc3d8ce611701a360cdcd69519148eab6cc
cakeprogramming12 <110796808+cakeprogramming12@users.noreply.github.com>
2025-04-27 21:50:49 -0600

    settings configuracion
    confiiguraciones arregladas

--- modules/modulo_regulaciones/apps.py
+++ modules/modulo_regulaciones/apps.py
@@ -1,6 +1,4 @@
 from django.apps import AppConfig
 
-
 class ModuloRegulacionesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'modulo_regulaciones'
+    name = 'modules.modulo_regulaciones'
