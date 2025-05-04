a7910570d07364093086ac89e25b3d78f67c92c1
NeftaliGC <aliguiacruz@outlook.com>
2025-05-04 01:40:50 -0600

    feat: pagina 404 personalizada en content feat: desplegable en Servicios feat: Paginas de pagar multa y verificación
    Se creo una template 404 personalizada que puede ser usada cuando es posible que un usuario intenta acceder a un recurso
    esperado que no existe, la misma acepta un poco de configuracion como el tipo de erro 404 sucedido y la redireccion de
    a donde podemos enviar al usuario.
    
    El desplegable en servicios es para poder añadir un punto de acceso a los distintos servicios como pagos, agendar cita, etc
    
    Las paginas de pagos estan listas para añadir funcionalidad directa.

--- modules/modulo_pagos/urls.py
+++ modules/modulo_pagos/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 
     path('', views.pagos, name='pagos'),
+    path('pagar/<str:tipo_pago>/', views.pagar, name='pagar'),
 
 ]
--- modules/modulo_pagos/views.py
+++ modules/modulo_pagos/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def pagos(request):
     return render(request, './pagos.html')
+
+def pagar(request, tipo_pago):
+    if tipo_pago == 'multa':
+        return render(request, './pagar_multa.html')
+    elif tipo_pago == 'verificacion':
+        return render(request, './pagar_verificacion.html')
+    else:
+        return render(request, './404_pago.html')
